e6910f97043f704538ecd79e740858db69bae1b0
Yunyang Lu <yunyang5@ualberta.ca>
2023-01-07 18:11:15 -0700

    modified template match

--- TemplateMatch.py
+++ TemplateMatch.py
@@ -1,27 +1,24 @@
 import cv2 as cv
-from matplotlib import pyplot as plt
 
 def TemplateMatch (source, temp):
     img = cv.imread(source, 0)
     template = cv.imread(temp, 0)
-    w, h = template.shape[::-1]
+    w, h = img.shape[::-1]
 
     method = eval('cv.TM_SQDIFF')
     # Apply template Matching
     res = cv.matchTemplate(img, template, method)
     min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
-    if method in [cv.TM_SQDIFF, cv.TM_SQDIFF_NORMED]:
-        top_left = min_loc
-    else:
-        top_left = max_loc
-    bottom_right = (top_left[0] + w, top_left[1] + h)
-    cv.rectangle(img, top_left, bottom_right, 255, 2)
-    plt.subplot(121), plt.imshow(res, cmap='gray')
-    plt.title('Matching Result'), plt.xticks([]), plt.yticks([])
-    plt.subplot(122), plt.imshow(img, cmap='gray')
-    plt.title('Detected Point'), plt.xticks([]), plt.yticks([])
-    plt.suptitle('cv.TM_SQDIFF')
-    plt.show()
+    top_left = min_loc
+
+    img_rev = cv.rotate(img, cv.ROTATE_180)
+    temp_rev = cv.rotate(template, cv.ROTATE_180)
+    method = eval('cv.TM_SQDIFF')
+    # Apply template Matching
+    res = cv.matchTemplate(img_rev, temp_rev, method)
+    min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+    #print(w, h)
+    #print(min_loc)
+    bottom_right = (w - min_loc[0], h-min_loc[1])
     #print(top_left, bottom_right)
     return top_left, bottom_right
