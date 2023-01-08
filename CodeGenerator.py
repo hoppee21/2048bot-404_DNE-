f05a51ef277ab6e4d1efdafe63fbf6907fb01f50
Yunyang-Lu <90409987+Yunyang-Lu@users.noreply.github.com>
2023-01-07 22:31:09 -0700

    Delete CodeGenerator.py

--- CodeGenerator.py
+++
@@ -1,4 +0,0 @@
-for operation in ['up', 'down', 'left', 'right']:
-    print("elif (OP == '" + operation + "'):")
-    print("\tkeyboard.press(Key." + operation + ")")
-    print("\tkeyboard.release(Key." + operation + ")")
